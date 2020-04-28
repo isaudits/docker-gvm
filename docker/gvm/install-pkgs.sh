e9e84a1b4e27e90ffc631d30913ea99c7f8b0c43
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2020-04-28 16:03:52 -0400

    combine GVM and GVMD image layers

--- docker/gvm/install-pkgs.sh
+++ docker/gvm/install-pkgs.sh
@@ -2,6 +2,22 @@
 
 apt-get update && apt-get upgrade -y
 
+#gvmd dependencies
+{ cat <<EOF
+gnutls-bin
+libglib2.0
+libgnutls28-dev
+libical2-dev
+nsis
+postgresql-$PG_MAJOR
+rsync
+texlive-fonts-recommended
+texlive-latex-extra
+xml-twig-tools
+xsltproc
+EOF
+} | xargs apt-get install -yq --no-install-recommends
+
 # gsa dependencies
 { cat <<EOF
 gnutls-bin
--- docker/gvmd/root/defaults/update.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/with-contenv bash
-
-echo "Updating NVTs..."
-s6-setuidgid abc greenbone-nvt-sync
