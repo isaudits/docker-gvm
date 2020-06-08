fde615d8f02c1565f38a58c869d23808f3c5e3d5
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2020-06-08 13:42:46 -0400

    manually rsync NVT plugin data to fix greenbone-nvt-sync hanging errors

--- docker/gvm/root/defaults/update-openvas.sh
+++ docker/gvm/root/defaults/update-openvas.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/with-contenv bash
 
 echo "Updating NVTs..."
-s6-setuidgid abc greenbone-nvt-sync
+#s6-setuidgid abc greenbone-nvt-sync
+s6-setuidgid abc rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /usr/local/var/lib/openvas/plugins
 sleep 5
 
 echo "Updating CERT data..."
--- docker/openvas/root/defaults/update-openvas.sh
+++ docker/openvas/root/defaults/update-openvas.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/with-contenv bash
 
 echo "Updating NVTs..."
-s6-setuidgid abc greenbone-nvt-sync
+#s6-setuidgid abc greenbone-nvt-sync
+s6-setuidgid abc rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /usr/local/var/lib/openvas/plugins
