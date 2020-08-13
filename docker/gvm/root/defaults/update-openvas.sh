22fd41e85ea599243e2c8d7090b680495f5b77a6
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2020-08-13 13:25:23 -0400

    add feed lock removal to update scripts

--- docker/gvm/root/defaults/update-openvas.sh
+++ docker/gvm/root/defaults/update-openvas.sh
@@ -5,9 +5,16 @@ echo "Updating NVTs..."
 s6-setuidgid abc rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /usr/local/var/lib/openvas/plugins
 sleep 5
 
+rm /usr/local/var/run/feed-update.lock || true
+
 echo "Updating CERT data..."
 s6-setuidgid abc greenbone-certdata-sync
 sleep 5
 
+rm /usr/local/var/run/feed-update.lock || true
+
 echo "Updating SCAP data..."
 s6-setuidgid abc greenbone-scapdata-sync
+sleep 5
+
+rm /usr/local/var/run/feed-update.lock || true
--- docker/openvas/root/defaults/update-openvas.sh
+++ docker/openvas/root/defaults/update-openvas.sh
@@ -3,3 +3,6 @@
 echo "Updating NVTs..."
 #s6-setuidgid abc greenbone-nvt-sync
 s6-setuidgid abc rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /usr/local/var/lib/openvas/plugins
+sleep 5
+
+rm /usr/local/var/run/feed-update.lock || true
